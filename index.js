229b7bbdc59d33302afff572e8320ed44cf0a047
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-05-06 23:44:41 +0600

    implement jwt in myItems and verify it secure it,, implement delivered and stock

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+  });
+  console.log("inside jwt", authHeader);
+  next();
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poetz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -54,15 +71,16 @@ async function run() {
       res.send(result);
     });
 
-    app.put("management/:id", async (req, res) => {
+    app.put("/management/:id", async (req, res) => {
       const id = req.params.id;
-      const quantity = req.body;
+      const updateQuantity = req.body;
+      // const stockQuantity = req.body;
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
-          quantity: quantity - 1,
-          // restock: quantity + 1,
+          quantity: updateQuantity.quantity,
+          // quantity: stockQuantity.quantity,
         },
       };
       const result = await warehouseCollection.updateOne(
@@ -73,18 +91,17 @@ async function run() {
       res.send(result);
     });
 
-    app.put("management/:data", async (req, res) => {
-      const data = req.params.data;
-      const quantity = req.body;
-      const filter = { _id: ObjectId(data) };
+    app.put("/management/:id", async (req, res) => {
+      const id = req.params.id;
+      const stockQuantity = req.body;
+      const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
-          // quantity: quantity - 1,
-          quantity: quantity + 1,
+          quantity: stockQuantity.quantity,
         },
       };
-      const result = await warehouseCollection.updateOne(
+      const result = await warehouseCollection.findOneAndUpdate(
         filter,
         updateDoc,
         options
@@ -92,12 +109,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/item", async (req, res) => {
+    app.get("/item", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = itemCollection.find(query);
-      const myItems = await cursor.toArray();
-      res.send(myItems);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = itemCollection.find(query);
+        const myItems = await cursor.toArray();
+        res.send(myItems);
+      } else {
+        res.status(403).send({ message: "Forbidden access" });
+      }
     });
 
     app.post("/item", async (req, res) => {
