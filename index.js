9ac1fbb3ab776f9bc8c925022f62f65697d23911
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2023-08-20 20:14:57 +0600

    updated

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
     const itemCollection = client.db("warehouse").collection("item");
     const suppliersCollection = client.db("warehouse").collection("supplier");
     const reviewCollection = client.db("warehouse").collection("review");
+    const userCollection = client.db("warehouse").collection("user");
 
     app.post("/login", async (req, res) => {
       const user = req.body;
@@ -106,6 +107,22 @@ async function run() {
     //   );
     //   res.send(result);
     // });
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.send({ result, token });
+    });
 
     app.get("/item", verifyJWT, async (req, res) => {
       const decodedEmail = req.decoded.email;
